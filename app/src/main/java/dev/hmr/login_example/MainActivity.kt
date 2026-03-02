5d06aaeccee29c2fe189cf386de4a756dffc0aa4
heitormrosi <heitor2509@outlook.com>
2026-03-02 18:16:49 -0300

    feat: Criação da tela de dashboard. Falta criar interação.

--- app/src/main/java/dev/hmr/login_example/MainActivity.kt
+++ app/src/main/java/dev/hmr/login_example/MainActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dev.hmr.login_example.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(this.binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        initListeners()
+    }
+
+    fun initListeners() {
+        this.binding.loginButton.setOnClickListener {
+
         }
     }
 }
